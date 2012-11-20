6d03761e9b256cec5709bb7c4a64a443163b8b32
m.bartolozzi <m.bartolozzi>
2012-11-20 10:38:39 +0000

    *** empty log message ***

--- abi/src/it/inera/abi/logic/TrasferimentoBiblioteca.java
+++ abi/src/it/inera/abi/logic/TrasferimentoBiblioteca.java
@@ -7,10 +7,10 @@ package it.inera.abi.logic;
  */
 public interface TrasferimentoBiblioteca {
 	
-	public void salva(int idbiblio) throws Exception ;
+	public void salva(int idbiblio) throws Exception;
 	
-	public void ripristina(int idbiblio) throws Exception ;
+	public void ripristina(int idbiblio) throws Exception;
 	
-	public void backupXml(int idbiblio) throws Exception ;
+	public void backupXml(int idbiblio) throws Exception;
 	
 }
